177f5a0b25871200593b22e83f36ffb14c087925
Dmytro-Pinchuk <dmytroit22@gmail.com>
2022-12-01 13:08:33 +0200

    Complete tasks 1 & 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,12 @@
-const catig = document.querySelectorAll("li.item");
-console.log(catig);
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories:`, categories.length);
 
+categories.forEach(getTitleAndList);
+
+function getTitleAndList(category) {
+    const categoryTitle = category.firstElementChild;
+    console.log(`Category:`, categoryTitle.textContent);
+
+    const categoryList = categoryTitle.nextElementSibling;
+    console.log(`Elements:`, categoryList.childElementCount);
+};
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+// const elements = ingredients.map(ingredient => {
+//   const liEl = document.createElement('li');
+//   liEl.classList.add("item");
+//   liEl.textContent = ingredient;
+
+//   return liEl;
+// });
+
+// console.log(elements);
+// // ingredientsEl.append(...elements);
+
+const createIngredientsList = options => {
+  return options.map(option => {
+    const liEl = document.createElement("li");
+    liEl.classList.add("item");
+    liEl.textContent = option;
+
+    return liEl;
+  })
+};
+
+const elements = createIngredientsList(ingredients);
+console.log(elements);
+ingredientsEl.append(...elements);
